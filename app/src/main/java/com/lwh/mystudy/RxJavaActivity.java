ada79514ab73e4f40f185e1f7ee7512d8cfdec84
liuwenhao <liuwenhao@kongzhongjr.com>
2018-05-03 11:45:22 +0800

    fix

--- app/src/main/java/com/lwh/mystudy/RxJavaActivity.java
+++ app/src/main/java/com/lwh/mystudy/RxJavaActivity.java
@@ -1,16 +1,29 @@
 package com.lwh.mystudy;
 
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 
 /**
@@ -27,40 +40,187 @@ public class RxJavaActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Observer<String> observer = new Observer<String>() {
+
+//        Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+//                Log.d(TAG, "emit 1");
+//                emitter.onNext(1);
+//                Log.d(TAG, "emit 2");
+//                emitter.onNext(2);
+//                Log.d(TAG, "emit 3");
+//                emitter.onNext(3);
+//                Log.d(TAG, "emit complete");
+//                emitter.onComplete();
+//                Log.d(TAG, "emit 4");
+//                emitter.onNext(4);
+//
+//
+//            }
+//        }).subscribe(new Observer<Integer>() {
+//            private Disposable mDisposable;
+//            private int i;
+//
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                Log.d(TAG, "subscribe");
+//                mDisposable = d;
+//            }
+//
+//            @Override
+//            public void onNext(Integer integer) {
+//                Log.d(TAG, "onNext: " + integer);
+//                i++;
+//                if (i == 2) {
+//                    Log.d(TAG, "dispose");
+//                    mDisposable.dispose();
+//                    Log.d(TAG, "isDisposed : " + mDisposable.isDisposed());
+//                }
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.d(TAG, "error");
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                Log.d(TAG, "complete");
+//            }
+//        });
+
+//
+//        Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+//                emitter.onNext(1);
+//                emitter.onNext(2);
+//                emitter.onNext(3);
+//                emitter.onNext(4);
+//                emitter.onComplete();
+//
+//            }
+//        }).subscribe(new Consumer<Integer>() {
+//            @Override
+//            public void accept(Integer integer) throws Exception {
+//                Log.e(TAG,integer + "");
+//            }
+//        }, new Consumer<Throwable>() {
+//            @Override
+//            public void accept(Throwable throwable) throws Exception {
+//                Log.e(TAG,throwable.toString() + "");
+//
+//            }
+//        }, new Action() {
+//            @Override
+//            public void run() throws Exception {
+//                Log.e(TAG,"run" + Thread.currentThread().getName());
+//            }
+//        });
+
+//        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+//                Log.e(TAG, "Observable thread is : " + Thread.currentThread().getName());
+//                Log.e(TAG, "emit 1");
+//                emitter.onNext(1);
+//            }
+//        });
+//        observable.subscribeOn(Schedulers.newThread())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Consumer<Integer>() {
+//                    @Override
+//                    public void accept(Integer integer) throws Exception {
+//                        Log.e(TAG,integer + " " + Thread.currentThread().getName() );
+//                    }
+//                });
+
+//        Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+//                e.onNext(1);
+//                e.onNext(2);
+//                e.onNext(3);
+//            }
+//        }).map(new Function<Integer, String>() {
+//            @Override
+//            public String apply(Integer integer) throws Exception {
+//                return "result" + integer;
+//            }
+//        }).subscribe(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) throws Exception {
+//                Log.e(TAG,s);
+//            }
+//        });
+
+//        Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+//                e.onNext(1);
+//                e.onNext(2);
+//                e.onNext(3);
+//            }
+//        }).flatMap(new Function<Integer, ObservableSource<String>>() {
+//            @Override
+//            public ObservableSource<String> apply(Integer integer) throws Exception {
+//                List<String> list = new ArrayList<>();
+//                for(int i = 0; i < 3; i++ ){
+//                    list.add("I am value " + integer);
+//                }
+//                return Observable.fromIterable(list).delay(10, TimeUnit.MILLISECONDS);
+//            }
+//        }).subscribe(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) throws Exception {
+//                Log.e(TAG, s);
+//            }
+//        });
+
+        Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+            }
+        });
+
+        Observable<String> observable2 = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                e.onNext("observable2+1");
+                e.onNext("observable2+2");
+                e.onNext("observable2+3");
+            }
+        });
+
+        Observable.zip(observable1, observable2, new BiFunction<Integer, String, String>() {
+            @Override
+            public String apply(Integer integer, String s) throws Exception {
+                return integer + s;
+            }
+        }).subscribe(new Observer<String>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.d(TAG,"onSubscribe");
+                Log.e(TAG, "onSubscribe");
             }
 
             @Override
             public void onNext(String s) {
-                Log.d(TAG,s);
+                Log.e(TAG, s);
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.d(TAG,"onError");
+                Log.e(TAG, e.toString());
             }
 
             @Override
             public void onComplete() {
-                Log.d(TAG,"onComplete");
-            }
-        };
-
-        Observable observable = Observable.create(new ObservableOnSubscribe() {
-            @Override
-            public void subscribe(ObservableEmitter e) throws Exception {
-                e.onNext("1");
-                e.onNext("2");
-                e.onNext("3");
-                e.onNext("4");
-                e.onComplete();
+                Log.e(TAG, "onComplete");
             }
         });
-        //订阅事件
-        observable.subscribe(observer);
 
     }
 }
